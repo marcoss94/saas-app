9616a0fe2f5b03586594deb131739b46a918a8fe
marcos <marcos.alvarez@tolber.io>
2025-05-31 20:34:15 -0300

    add navbar

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ const Page = () => {
   return (
     <div>
       <h1 className="text-2xl underline">Welcome to my SaaS App</h1>
-      <Button>Let's get started!</Button>
+      <Button>Let s get started!</Button>
     </div>
   );
 };
